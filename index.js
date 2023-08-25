b6758c1555b7d1332a42dbdc43352aa55d40d89a
EduENL <eduardonuneslima099@gmail.com>
2023-08-25 14:35:17 -0300

    1.5_calendario_pronto
    finalização do calendário e responsividade arrumada da home

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var scrollPosition = 0;
 
 $(".carousel-control-next").on("click", function () {
     if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-      scrollPosition += cardWidth;  //update scroll position
+      scrollPosition += cardWidth * 3;  //update scroll position
       $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
     }
   });
 
   $(".carousel-control-prev").on("click", function () {
     if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
+      scrollPosition -= cardWidth * 3;
       $(".carousel-inner").animate(
         { scrollLeft: scrollPosition },
         600
