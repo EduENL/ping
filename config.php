3610f806e6e1ee025a7e266e07d08ebc02ce697e
felipe0612 <Felipe.bp1210@gmail.com>
2023-11-30 13:29:21 -0300

    config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$dbhost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'formulario_ping';
+
+
+$conexao = new mysqli($dbhost,$dbUsername,$dbPassword,$dbName);
+
+if($conexao->connect_errno)
+{
+    echo "Erro";
+}
+else
+{
+    echo "Conexão efetuada com sucesso";
+}
+?>
