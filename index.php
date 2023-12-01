59916674952cf20a7ae78ec1b329a950063330eb
felipe0612 <Felipe.bp1210@gmail.com>
2023-12-01 12:51:48 -0300

    inicio_sessions2

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+// print_r($_SESSION);
+
+if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
+{
+    unset($_SESSION['email']);
+    unset($_SESSION['senha']);
+    header('Location: Login copy.php');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
