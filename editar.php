4b6f15ded789b51d14b5aec1668fd6ab8539533a
EduENL <eduardonuneslima099@gmail.com>
2023-12-07 13:47:16 -0300

    aaaaaaaaaaaaaaaaaaaaa

--- editar.php
+++ editar.php
@@ -16,7 +16,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="editar.css">
-
     <title>Bem-Vindo!</title>
 </head>
 
